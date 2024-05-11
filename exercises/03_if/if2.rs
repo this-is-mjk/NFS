42830715eb5b09a5e623b334af5ac3f63125f4af
this-is-mjk <mjlivetowork@gmail.com>
2024-05-11 18:14:54 +0530

    done till ex4

--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -3,15 +3,15 @@
 // Step 1: Make me compile!
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 //
-// Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// DONE
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
