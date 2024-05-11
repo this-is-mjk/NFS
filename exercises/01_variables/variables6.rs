6d71be18b207c1938946c8a4bc265389e2c690ab
this-is-mjk <mjlivetowork@gmail.com>
2024-05-11 16:29:50 +0530

    first commit

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -1,11 +1,7 @@
 // variables6.rs
-//
-// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
-// hint.
+// DONE
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
